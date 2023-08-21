f1713c4e71616832762b3fa707a4751f99d44267
Volume999 <Volume99917@gmail.com>
2023-08-21 20:33:14 +0600

    Added task details modal

--- Backend/main.go
+++ Backend/main.go
@@ -2,6 +2,14 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
+/*
+TODO:
+ 1. install MongoDB
+ 2. Create Models, Database, and Routes
+ 3. Add task button
+ 4. Check task button
+ 5. Delete task button
+*/
 func main() {
 	app := fiber.New()
 
